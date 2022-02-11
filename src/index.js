c3a3da3d8235f1aed6dd63859cc577efeee4d16b
easedeveloper <sespinozaea29@gmail.com>
2022-02-10 22:31:17 -0500

    agregar cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express= require('express');
 const bodyParser = require('body-parser');
 //const PORT = process.env.PORT || 3050;
 
-const cors = require(cors);
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
