fd1323dc5501f97d35aacd943465f08f54e9a573
easedeveloper <sespinozaea29@gmail.com>
2022-02-10 22:48:34 -0500

    nueva actualizacion

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ app.use( express.urlencoded({ extended: true }) );
 
 app.use( cors() );
 
-// AÑADIENDO Routes DESDE ./routes/routesindex
+//rutas
+app.use(require("./src/Routes/index"))
+
+app.get('/',(req, res) => {res.send("Welcome to E_COMMERCE API-REST")})
+//ejecucion
+//app.listen(3000)
+app.listen(process.env.PORT || 5050)
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+console.log("Server running in http://localhost:5050")
+
+
+/* // AÑADIENDO Routes DESDE ./routes/routesindex
 app.use( require('./src/routes/routes-index') );
 
 //EJECUTA EN EL PUERTO 3000
@@ -30,4 +41,4 @@ app.connect(error =>{
 
 app.listen(process.env.PORT || 5050)
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-console.log("Server running in http://localhost:5050")
+console.log("Server running in http://localhost:5050") */
